282e69d9519455eb365b377574e531741555ff5b
Bernd Schray <bernd.schray@beejees.de>
2017-11-30 22:53:05 +0100

    added travel-site

---
+++ travel-site/gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync').create();
+
+gulp.task('styles', function(){
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe(postcss([nested, cssvars, autoprefixer]))
+	.pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function(){
+
+	browserSync.init({
+		server: {
+			baseDir: "app"
+		}
+	});
+
+	watch('./app/index.html', function(){
+		browserSync.reload();
+	});
+	watch('./app/assets/styles/**/*.css', function(){
+		gulp.start('cssInject');
+	});
+});
+
+gulp.task('cssInject', ['styles'], function() {
+	return gulp.scr('./app/temp/styles/styles.css')
+	.pipe(browserSync.stream());
+});
