958acb940f9b6f21b41bfab0928b9a2b2364cfef
Bernd Schray <bernd.schray@beejees.de>
2017-12-01 14:37:32 +0100

    gulp/tasks files added

--- travel-site/gulpfile.js
+++ travel-site/gulpfile.js
@@ -1,35 +1,6 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import'),
-browserSync = require('browser-sync').create();
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('styles', function(){
-	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([nested, cssvars, autoprefixer]))
-	.pipe(gulp.dest('./app/temp/styles'));
-});
 
-gulp.task('watch', function(){
 
-	browserSync.init({
-		server: {
-			baseDir: "app"
-		}
-	});
 
-	watch('./app/index.html', function(){
-		browserSync.reload();
-	});
-	watch('./app/assets/styles/**/*.css', function(){
-		gulp.start('cssInject');
-	});
-});
-
-gulp.task('cssInject', ['styles'], function() {
-	return gulp.scr('./app/temp/styles/styles.css')
-	.pipe(browserSync.stream());
-});
